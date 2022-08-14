7d16d37f57bb64d9015b94ebe7ced8f1d0176448
paulo.tavares <paulo.tavares@olxbr.com>
2022-08-14 17:38:16 -0300

    DSL para datas

---
+++ src/main/kotlin/data/DataUtil.kt
@@ -0,0 +1,15 @@
+package data
+import java.time.LocalDate
+import java.time.Period
+
+val Int.days: Period
+    get() = Period.ofDays(this)
+val Period.ago: LocalDate
+    get() = LocalDate.now() - this
+val Period.fromNow: LocalDate
+    get() = LocalDate.now() + this
+
+fun main(args : Array<String>){
+    println(1.days.ago)
+    println(2.days.fromNow)
+}
